b682b61a791176b68f2604ba13f455bc3c5d8375
qulv-feng <ekag18792176851@163.com>
2025-03-09 00:21:32 +0800

    Record the way I have learned about Language C/C++

---
+++ 字符串处理函数/test.1.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+void str_toggle_case(char *str)
+int str_compare(const char *str1, const char *str2)
+int is_palindrome(const char *str)
+int is_numeric(const char *str)
+char* big_number_calculator(const char *num1, const char *num2, char op)
