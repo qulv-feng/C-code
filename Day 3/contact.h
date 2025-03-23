9a9c1d8707c15d26452212e1c29504a90506cedb
qulv-feng <ekag18792176851@163.com>
2025-03-23 23:48:56 +0800

    To Study language C

---
+++ Day 3/contact.h
@@ -0,0 +1,68 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#define MAX 100
+
+#define MAX_NAME 20
+#define MAX_GENDER 10
+#define MAX_TEL 30
+#define MAX_ADD 50
+
+#define DEFAULT_SZ 3
+#define INC_SZ 2
+
+
+typedef struct Pelinfo
+{
+	char name[MAX_NAME];
+	int age;
+	char gender[MAX_GENDER];
+	char tel[MAX_TEL];
+	char address[MAX_ADD];
+}Peoinfo;
+
+
+
+typedef struct Contact
+{
+	Peoinfo* date;
+	int count;
+	int capacity;
+}Contact;
+
+
+void AddCon(Contact* p);
+
+void InitCon(Contact* p);
+
+void SearchCon(Contact* p);
+
+void ShowCon(Contact* p);
+
+void Modify(Contact* p);
+
+void DeleteCon(Contact* p);
+
+void SortCon(Contact* p);
+
+int FindName(Contact* p, char* str);
+
+void CheckCapacity(Contact* p);
+
+void DestoryContact(Contact* p);
+
+void SaveContact(Contact* pc);
+
+void LoadContact(Contact* pc);
+
+//typedef struct Contact
+//{
+//	Peoinfo date[MAX];
+//	int count;
+//}Contact;
---
+++ Day 3/test.c
@@ -0,0 +1,65 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include "contact.h"
+
+void menu()
+{
+	printf("**************************************************************************\n");
+	printf("*************************1.add          2.search**************************\n");
+	printf("*************************3.show         4.modify**************************\n");
+	printf("*************************5.delete       6.sort****************************\n");
+	printf("*************************0.exit         0.exit****************************\n");
+	printf("**************************************************************************\n");
+}
+
+enum Option
+{
+	EXIT,
+	ADD,
+	SEARCH,
+    SHOW,
+	MODIFY,
+	DELETE,
+	SORT,
+};
+
+int main()
+{
+	int input = 0;
+	Contact con;
+	InitCon(&con);
+	do
+	{
+		menu();
+		printf("请选择->");
+		scanf_s("%d", &input);
+		switch (input)
+		{
+		case EXIT:
+			DestoryContact(&con);
+			printf("退出通讯录\n");
+			break;
+		case ADD:
+			AddCon(&con);
+			break;
+		case SEARCH:
+			SearchCon(&con);
+			break;
+		case SHOW:
+			ShowCon(&con);
+			break;
+		case MODIFY:
+			Modify(&con);
+			break;
+		case DELETE:
+			DeleteCon(&con);
+			break;
+		case SORT:
+			SortCon(&con);
+			break;
+		default:
+			printf("选择错误,重新选择\n");
+			break;
+		}
+	} while (input);
+	return 0;
+}
