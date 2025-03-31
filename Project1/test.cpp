de6b4618b730b7f77002972622df4f0157e0c3fa
qulv-feng <ekag18792176851@163.com>
2025-03-31 22:17:23 +0800

    1

---
+++ Project1/test.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	char ch;
+//	cin >> ch;
+//	cout << "  " << ch<< "  " << endl;
+//	cout << " " << ch << ch << ch << " " << endl;
+//	cout << ch << ch << ch << ch << ch << endl;
+//	return 0;
+//}
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a = 0;
+	short b = 0;
+	cout << sizeof(int) << " " << sizeof(short) << endl;
+	return 0;
+}
