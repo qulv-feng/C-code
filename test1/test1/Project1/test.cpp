de9dd4b637bb1c5bc818a5c7e93dee960a747a83
qulv-feng <ekag18792176851@163.com>
2025-04-02 21:45:13 +0800

    1

---
+++ test1/test1/Project1/test.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+using namespace std;
+//int main()
+//{
+//	string s1 = "abcdef";
+//	
+//	for (string::iterator it = s1.end()-1;it >= s1.begin();it--)
+//	{
+//		cout << *it << " ";
+//		if (it == s1.begin())
+//			break;
+//	}
+//	return 0;
+//}
+
+/*string::iterator it1 = s1.begin();
+	string::iterator it2 = s1.end();*/
+	/*cout << *it1 << endl;*/
+
+//int main()
+//{
+//	string s1 = "hello";
+//	s1.pop_back();
+//	cout << s1 << endl;
+//}
+
+//int main()
+//{
+//	string s1 = "hello world,hello everyone";
+//	string s2 = "hello";
+//	cout << s1 << endl;
+//	s1.insert(5, s2);
+//	cout << s1 << endl;
+//	s1.insert(5, "hello");
+//	cout << s1 << endl;
+//	s1.insert(5, 2, 'h');
+//	cout << s1 << endl;
+//	int n1 = s1.find('x', 0);
+//	int n2 = s1.find("hello", n1 + 1);
+//	int n3 = s1.find("hello", n2 + 1, 3);
+//	int n4 = s1.find('y', 0);
+//	cout << n1 << " " << n2 << " " << n3 << " " << n4 << endl;
+//
+//	if (n1 == string::npos)
+//		printf("No this string\n");	
+//	else
+//		printf("the pos is %d\n", n1);
+//
+//	return 0;
+//}
+
